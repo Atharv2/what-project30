155ebed8b38a227c8387f0d299078759f63ab761
atharv2c <73940264+Atharv2@users.noreply.github.com>
2020-12-30 18:38:48 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# what-project30
+# AngryBirdsStage3
+Stage 3 Angry Birds: Introducing Constraint
---
+++ sketch.js
@@ -0,0 +1,79 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, pig1;
+var backgroundImg,platform;
+var bird, slingShot;
+
+function preload() {
+    backgroundImg = loadImage("sprites/bg.png");
+}
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+
+    ground = new Ground(1100,400,1000,300);
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810, 350);
+    log1 = new Log(810,260,300, PI/2);
+    log4=new Log(800,300,300,PI/2);
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    pig2= new Pig(810, 220);
+    pig3 = new Pig(800,220)
+   
+   
+    bird = new Bird(200,50);
+
+    //log6 = new Log(230,180,80, PI/2);
+    slingshot = new SlingShot(bird.body,{x:200, y:50});
+}
+
+function draw(){
+    background(backgroundImg);
+    Engine.update(engine);
+    //strokeWeight(4);
+    box1.display();
+    box2.display();
+    ground.display();
+    pig1.display();
+    log1.display();
+     pig3.display();
+    box3.display();
+    box4.display();
+    pig2.display();
+    
+    log4.display();
+   
+    
+    bird.display();
+    
+    //log6.display();
+    slingshot.display();    
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+}
+
+
+function mouseReleased(){
+    slingshot.fly();
+}
+function keyPressed()
+{
+    if (keyCode===32)
+    {
+        slingshot.attach(bird.body);
+    }
+}
+
+
